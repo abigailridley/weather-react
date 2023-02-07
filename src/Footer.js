2881a4895b6a3fcc2b9c3269664ab9ef19329aa5
abigailridley <abigail.ridley@hotmail.co.uk>
2023-02-07 11:11:53 +0000

    Update footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,6 @@ import "./Footer.css"
 export default function Footer() {
     return (
     <footer>
-        <small><a href="https://github.com/abigailridley/weather-react" target="_blank" rel="noreferrer">Open Source</a> by Abigail Ridley</small>
+        <p><a href="https://github.com/abigailridley/weather-react" target="_blank" rel="noreferrer">Open Source</a> by Abigail Ridley</p>
     </footer>)
 }
